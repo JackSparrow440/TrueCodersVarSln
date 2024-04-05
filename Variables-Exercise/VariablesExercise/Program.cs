29182fb9d28086c45b2e66169f880d7e3510a43b
Jack Sparrow <91615638+JackSparrow440@users.noreply.github.com>
2024-04-05 15:30:19 -0500

    Add files via upload
    Variables assignment.

---
+++ Variables-Exercise/VariablesExercise/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace VariablesExercise
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string dogName = "dog";
+            int dogAge = 10;
+            char dogChar = 'd';
+            bool isDog = true;
+            double ageInDays = 3658;
+            decimal ageInMinutes = 5267520.992762M;
+            Console.WriteLine($"Dog name is {dogName}, his Age in years is: {dogAge}");
+            Console.WriteLine($"His Age in Days is {ageInDays}, his Age in minutes is: {ageInMinutes} and First char of his name is {dogChar}");
+            Console.WriteLine($"My Pet is: " + (isDog ? "dog" : "cat"));
+        }
+    }
+}
